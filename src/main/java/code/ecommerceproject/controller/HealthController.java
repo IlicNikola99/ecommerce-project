1a76abb39cc95ae76f6db14f3e95f573c75527fc
NikolaIlic99 <ilic.nikolabs@gmail.com>
2024-12-21 18:59:31 +0100

    Initial Commit

---
+++ src/main/java/code/ecommerceproject/controller/HealthController.java
@@ -0,0 +1,17 @@
+package code.ecommerceproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping()
+    public String checkHealth() {
+        return "All Ok";
+    }
+}
