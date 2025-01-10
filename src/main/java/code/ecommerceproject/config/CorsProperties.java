600cb9af2e1fb2d47ed06a657b2d3a73daf46cd2
NIlic <ilic.nikolabs@gmail.com>
2025-01-10 12:48:32 +0100

    Added CORS filter

---
+++ src/main/java/code/ecommerceproject/config/CorsProperties.java
@@ -0,0 +1,17 @@
+package code.ecommerceproject.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+public class CorsProperties {
+
+    @Bean
+    @ConfigurationProperties(prefix = "application.cors", ignoreInvalidFields = true)
+    public CorsConfiguration corsConfiguration() {
+        return new CorsConfiguration();
+    }
+
+}
