fdc526309b5e4bba2a36c53aea15b4317c0a6677
NIlic <ilic.nikolabs@gmail.com>
2025-02-04 00:02:28 +0100

    Added logging

--- src/main/java/code/ecommerceproject/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/code/ecommerceproject/exception/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package code.ecommerceproject.exception.handler;
 import code.ecommerceproject.exception.ErrorDetails;
 import code.ecommerceproject.exception.MultipartPictureException;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     // Handle specific exceptions
@@ -29,6 +31,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDetails> handleGlobalException(Exception ex, WebRequest request) {
         final ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), request.getDescription(false));
+        log.error(ex.getMessage(), ex);
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
