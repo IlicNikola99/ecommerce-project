657d112a5b44b816a32151fa49057edc7c45cf3a
NIlic <ilic.nikolabs@gmail.com>
2025-02-04 16:22:50 +0100

    Ignoring product mapping

--- src/main/java/code/ecommerceproject/mapper/CategoryMapper.java
+++ src/main/java/code/ecommerceproject/mapper/CategoryMapper.java
@@ -3,6 +3,7 @@ package code.ecommerceproject.mapper;
 import code.ecommerceproject.dto.CategoryDto;
 import code.ecommerceproject.entity.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -10,6 +11,7 @@ public interface CategoryMapper {
 
     CategoryMapper Instance = Mappers.getMapper(CategoryMapper.class);
 
+    @Mapping(target = "products", ignore = true)
     CategoryDto toDto(Category user);
 
     Category toEntity(CategoryDto userDTO);
